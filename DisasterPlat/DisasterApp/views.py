c3d792e59034b14d3a4ddb240fbec0b8d80eed95
Braison Wabwire <braisonwabwire2003@gmail.com>
2024-09-10 21:38:31 +0300

    adding authentiactions

--- DisasterPlat/DisasterApp/views.py
+++ DisasterPlat/DisasterApp/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 
 def index(request):
     context={'message':'Hello Braison'}
     return render(request,'index.html',context)
+
+def signup(request):
+    form=UserCreationForm()
+    context={'form':form}
+    return render(request,'signup.html',context)
